4e0f4178a033a33e6f2c5366a8a7f067d9880de1
Luiz Silva <luiz.silva@concrete.com.br>
2018-11-08 14:19:40 -0200

    Add traversal through ~/Library/Developer/XCTestDevices looking for snapshots

--- Sources/SnapshotFailLocator/SnapshotPathEnumerator.swift
+++ Sources/SnapshotFailLocator/SnapshotPathEnumerator.swift
@@ -3,16 +3,75 @@ import Cocoa
 
 class SnapshotPathEnumerator {
     static func enumerateSnapshotFiles() -> AnySequence<SnapshotFile> {
-        let snapshots =
-            DeviceIterator().lazy.flatMap { iterator in
-                return iterator.lazy
+        let coreDevices = CoreDevicesIterator()
+        let testDevices = XCTestDevicesIterator()
+        
+        var snapshots: [SnapshotFile] =
+            coreDevices.flatMap { iterator in
+                return iterator
             }
+        
+        snapshots.append(contentsOf:
+            testDevices.flatMap { iterator in
+                return iterator
+            })
         
         return AnySequence<SnapshotFile>(snapshots)
     }
 }
 
-struct DeviceIterator: Sequence, IteratorProtocol {
+struct XCTestDevicesIterator: Sequence, IteratorProtocol {
+
+    fileprivate var enumerator: FileManager.DirectoryEnumerator
+    fileprivate var iterator: NSFastEnumerationIterator
+    
+    init() {
+        let fileManager = FileManager.default
+        
+        let devicesPath = ("~/Library/Developer/XCTestDevices/" as NSString).standardizingPath
+        
+        guard let filesEnumerator =
+            fileManager.enumerator(at: URL(fileURLWithPath: devicesPath),
+                                   includingPropertiesForKeys: nil,
+                                   options: [.skipsHiddenFiles, .skipsPackageDescendants, .skipsSubdirectoryDescendants],
+                                   errorHandler: { (url, error) in
+                                    print("Error navigating \(url.relativePath): \(error)")
+                                    return false
+            }) else {
+                
+                fatalError("Error fetching enumerator for path \(devicesPath)!")
+        }
+        
+        enumerator = filesEnumerator
+        iterator = enumerator.makeIterator()
+    }
+    
+    mutating func next() -> ApplicationSnaphotIterator? {
+        while let url = iterator.next() as? URL {
+            let appUrl = url.appendingPathComponent("data/Containers/Data/Application")
+            
+            if FileManager.default.fileExists(atPath: appUrl.absoluteURL.path) {
+                
+                guard let filesEnumerator =
+                    FileManager.default.enumerator(at: appUrl,
+                                                   includingPropertiesForKeys: [.contentModificationDateKey],
+                                                   options: [.skipsHiddenFiles, .skipsPackageDescendants],
+                                                   errorHandler: { (url, error) in
+                                                    print("Error navigating \(url.relativePath): \(error)")
+                                                    return false
+                    }) else {
+                        fatalError("Error fetching enumerator for path \(appUrl)!")
+                }
+                
+                return ApplicationSnaphotIterator(enumerator: filesEnumerator)
+            }
+        }
+        
+        return nil
+    }
+}
+
+struct CoreDevicesIterator: Sequence, IteratorProtocol {
     
     fileprivate var enumerator: FileManager.DirectoryEnumerator
     fileprivate var iterator: NSFastEnumerationIterator
